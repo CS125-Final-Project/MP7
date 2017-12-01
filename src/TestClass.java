5a79c24dd85bbc1a6f6807ba7140b7cfc64ce602
dvg <dvg@wirelessprv-10-193-100-191.near.illinois.edu>
2017-12-01 14:26:36 -0600

    Using a modified google checkstyle configuration.

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
+/**
+ * It's a class. Yeah!
+ * 
+ * @author dvg
+ */
 public class TestClass {
-    public static void main(String[] args) {
+    /**
+     * Does stuff.
+     * 
+     * @param args unused
+     */
+    public static void main(final String[] args) {
         System.out.println("Do a thing.");
+        Scanner input = new Scanner(System.in);
+        if (input.nextInt() == 0) {
+            System.out.println("That's 0.");
+        } else {
+            System.out.println("That's not 0.");
+        }
+        input.close();
     }
 }
