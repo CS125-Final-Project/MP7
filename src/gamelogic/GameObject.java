a8e76bfa3fea460d5e341c01569b0561d9c2f41b
Elayda <amhou2@illinois.edu>
2017-12-01 16:59:56 -0600

    GameObject & package-info
    This commit adds an abstract game object which can be extended into
    different game objects like walls, floors, mobs, and the player.

---
+++ src/gamelogic/GameObject.java
@@ -0,0 +1,22 @@
+package gamelogic;
+
+
+/**
+ * This is the class which contains the basic properties for all immovable
+ * objects.
+ * @author Elayda
+ *
+ */
+public abstract class GameObject {
+    /**
+     * This is the character an object will show when it is printed.
+     */
+private char ascii = "";
+
+/**
+ * This method prints an obstacle's ascii character.
+ */
+public void printAscii() {
+    System.out.print(this.ascii);
+}
+}
