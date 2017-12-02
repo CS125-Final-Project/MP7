ea08eb4d13f80afcc6846391cac9264ac640d912
dvg <dvg@wirelessprv-10-193-100-191.near.illinois.edu>
2017-12-02 16:23:45 -0600

    Fixed formatting.

--- src/gamelogic/GameObject.java
+++ src/gamelogic/GameObject.java
@@ -11,19 +11,22 @@ public abstract class GameObject {
     /**
      * This is the character an object will show when it is printed.
      */
-private String ascii = " ";
-/**
- * This boolean controls whether a mob can enter this tile.
- */
-private boolean passable = false;
-/**
- * This is the maximum stack of items in the inventory or on the floor.
- */
-static final int MAX_STACK = 99;
-/**
- * This method prints an obstacle's ascii character.
- */
-public void printAscii() {
-    System.out.print(this.ascii);
-}
+    private String ascii = " ";
+    
+    /**
+     * This boolean controls whether a mob can enter this tile.
+     */
+    private boolean passable = false;
+    
+    /**
+     * This is the maximum stack of items in the inventory or on the floor.
+     */
+    static final int MAX_STACK = 128;
+    
+    /**
+     * This method prints an obstacle's ascii character.
+     */
+    public void printAscii() {
+        System.out.print(this.ascii);
+    }
 }
