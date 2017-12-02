30ab7ae1ad6105b39a66e859acdf54f04c3ad900
Elayda <amhou2@illinois.edu>
2017-12-01 18:03:30 -0600

    Game Object and wall classes
    This created a wall class and appropriate java documentation. Also added
    a passable boolean to the GameObject abstract class which will control
    whether a character can move.

--- src/gamelogic/GameObject.java
+++ src/gamelogic/GameObject.java
@@ -12,6 +12,10 @@ public abstract class GameObject {
      * This is the character an object will show when it is printed.
      */
 private String ascii = " ";
+/**
+ * This boolean controls whether a mob can enter this tile.
+ */
+private boolean passable = false;
 
 /**
  * This method prints an obstacle's ascii character.
---
+++ src/gamelogic/Wall.java
@@ -0,0 +1,13 @@
+/***/
+package gamelogic;
+
+/**
+ * @author Elayda
+ * This is the class used to denote that there is a wall.
+ */
+public class Wall extends GameObject {
+    /**
+     * This controls whether the wall can be broken.
+     */
+private boolean canBreak;
+}
