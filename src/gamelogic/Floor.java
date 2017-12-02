fdbcf925db3a995de838a1ed3feaad2773345b7f
dvg <dvg@wirelessprv-10-193-100-191.near.illinois.edu>
2017-12-02 16:56:00 -0600

    Minor changes.

--- src/gamelogic/Floor.java
+++ src/gamelogic/Floor.java
@@ -4,7 +4,6 @@ package gamelogic;
  * @author Elayda This is the class used to denote that there is a floor.
  */
 public class Floor extends GameObject {
-    private GameObject[] stack = new GameObject[64];
-    private int stackHeight;
-
+    // private GameObject[] stack = new GameObject[64];
+    // private int stackHeight;
 }
--- src/gamelogic/Player.java
+++ src/gamelogic/Player.java
@@ -11,12 +11,22 @@ package gamelogic;
 public class Player extends GameObject {
 
     /**
-     * has the player in question died.?
+     * Whether the player in question has died.
      */
     private boolean isAlive;
-
+    
+    /**
+     * Current item in use.
+     */
+    private GameObject item;
+    
     /**
      * this is how many items you can stack in a place.
      */
     private GameObject[] inventory = new GameObject[MAX_STACK];
+    
+    public Player() {
+        isAlive = true;
+        item = null;
+    }
 }
