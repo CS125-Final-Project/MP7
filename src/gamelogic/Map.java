705ddb6d848b0d093d5193b1c4cd8faf2c4374a3
Elayda <amhou2@illinois.edu>
2017-12-09 13:58:07 -0600

    this is a map
    It contains an integer array and GameObject array which can track the
    heat and position of objects

---
+++ src/gamelogic/Map.java
@@ -0,0 +1,11 @@
+package gamelogic;
+
+public class Map {
+    private int mapHeight;
+    private int mapWidth;
+    public int[][] heatMap;
+    public GameObject[][] gameMap;
+    
+
+    public Map(final int width, final int )
+}
