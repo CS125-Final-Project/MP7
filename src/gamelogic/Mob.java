4773992781b3051c0699c767499f6d0565e74f01
Elayda <amhou2@illinois.edu>
2017-12-09 15:19:53 -0600

    Mob
    redevised find player to use the heat map data.

--- src/gamelogic/Mob.java
+++ src/gamelogic/Mob.java
@@ -7,18 +7,18 @@ package gamelogic;
  *
  */
 public class Mob extends GameObject {
-    
+    private Map world;
     /** Is it trying to kill you.? */
     private boolean isHostile;
-    
+
     private int attackRange;
-    
+
     // Codes for directions
     public static int NORTH = 0;
     public static int EAST = 1;
     public static int SOUTH = 2;
     public static int WEST = 3;
-    
+
     /**
      * @return This will return an integer based on which step will lead to the
      *         player the fastest. 0 is a North, 1 is a East, 2 is a South, 3 is a
@@ -27,14 +27,27 @@ public class Mob extends GameObject {
     public int findPlayer(Player player) {
         // currently ignores walls
         int[] dirs = new int[4];
-        dirs[NORTH] = this.y - player.getY();
-        dirs[EAST] = player.getX() - this.x;
-        dirs[SOUTH] = player.getY() - this.x;
-        dirs[WEST] = this.x - player.getX();
-        
-        return Util.findIndexOfMax(dirs);
+        /*
+         * dirs[NORTH] = this.y - player.getY(); dirs[EAST] = player.getX() - this.x;
+         * dirs[SOUTH] = player.getY() - this.x; dirs[WEST] = this.x - player.getX();
+         * 
+         * 
+         * return Util.findIndexOfMax(dirs);
+         */
+        dirs[NORTH] = world.heatMap[x][y - 1];
+        dirs[EAST] = world.heatMap[x + 1][y];
+        dirs[SOUTH] = world.heatMap[x][y + 1];
+        dirs[WEST] = world.heatMap[x - 1][y];
+
+        int ind = 0;
+        for (int i = 0; i < dirs.length; i += 1) {
+            if (dirs[ind] > dirs[i]) {
+                ind = i;
+            }
+        }
+        return ind;
     }
-    
+
     /** Attacks the player. */
     public boolean attack() {
         return false;
