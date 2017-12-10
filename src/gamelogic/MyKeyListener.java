0e3d62cc0c4ced3d38d910d956ce8d3e37ba6c34
Elayda <amhou2@illinois.edu>
2017-12-09 19:10:23 -0600

    Implemented KeyListener

---
+++ src/gamelogic/MyKeyListener.java
@@ -0,0 +1,26 @@
+package gamelogic;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyKeyListener implements KeyListener {
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
