98cd8eba7c3f61f4416992f2d78f658bf48a6f2b
Elayda <amhou2@illinois.edu>
2017-12-09 19:40:55 -0600

    implemented key checking

--- src/gamelogic/MyKeyListener.java
+++ src/gamelogic/MyKeyListener.java
@@ -1,26 +1,39 @@
 package gamelogic;
 
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class MyKeyListener implements KeyListener {
+public class MyKeyListener extends KeyAdapter implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent arg0) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void keyReleased(KeyEvent arg0) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
-    public void keyTyped(KeyEvent arg0) {
+    public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
-        
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_W) {
+            
+        }
+        if (key == KeyEvent.VK_A) {
+            
+        }
+        if (key == KeyEvent.VK_S) {
+            
+        }
+        if (key == KeyEvent.VK_D) {
+            
+        }
     }
 
 }
