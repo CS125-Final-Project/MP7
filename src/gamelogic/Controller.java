b61932c97a81e13dd356a15d4347d078fb414a2f
dvg <dvg@wirelessprv-10-193-100-191.near.illinois.edu>
2017-12-09 19:40:42 -0600

    Set up controller to work with a key listener.

--- src/gamelogic/Controller.java
+++ src/gamelogic/Controller.java
@@ -7,19 +7,31 @@ public class Controller {
      * @param args unused
      */
     public static void main(final String[] args) {
-        Map map = new Map("Map_Data.txt");
-        Player player = map.getPlayer();
+        map = new Map("Map_Data.txt");
+        player = map.getPlayer();
+        input = new MyKeyListener();
+        /*
         while (player.isAlive()) {
             // get player's next move
             int nextMove = -1;
             while (!map.checkMove(nextMove)) {
                 nextMove = getMove();
-            }
-            
+            } 
         }
+        */
     }
     
+    private static Map map;
+    private static Player player;
+    private static MyKeyListener input;
+    
+    public static void movePlayer(int direction) {
+        // does some stuff
+    }
+    
+    /*
     private static int getMove() {
         return -1;
     }
+    */
 }
